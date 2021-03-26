66f91ccf16f69717489eec4c4ef1740d16bb9d55
annu12340 <annujolly17@gmail.com>
2021-03-26 12:40:31 +0530

    Get parameters from url

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
 from . import views
 
+app_name="leads"
+
 urlpatterns = [
-    path('',  views.home_page),
+    path('',  views.lead_list),
+    path('<pk>/',  views.lead_detail),
 ]
 
 
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from . models import Lead
 # Create your views here.
-def home_page(request):
+
+def lead_list(request):
     leads=Lead.objects.all()
-    return render(request,'leads/home_page.html',{'lead_data':leads})
+    return render(request,'leads/lead_list.html',{'lead_data':leads})
+
+def lead_detail(request,pk):
+    print(pk)
+    lead=Lead.objects.get(id=pk)
+    return render(request, 'leads/lead_list.html', {'lead_data': lead})
